ee2fb23a1decc793a5a160e0e507624461bec3d3
Uniiquee <44533127+Uniiquee@users.noreply.github.com>
2020-10-30 08:40:18 +0100

    Add new XML to JSON conversion method to the conversion service

--- src/main/java/com/example/JsonToXmlHttpService/JsonXmlConversionService.java
+++ src/main/java/com/example/JsonToXmlHttpService/JsonXmlConversionService.java
@@ -19,4 +19,16 @@ public class JsonXmlConversionService {
         return U.jsonToXml(json);
     }
 
+    /**
+     * Converts the provided XML into JSON.
+     * @s https://www.json.org/json-en.html
+     * @param xml the XML to be converted into JSON.
+     * @ a valid JSON document representing the specified XML.
+     * @see <a href="https://www.json.org/json-en.html">More information about JSON</a>
+     * @see <a href="https://www.w3.org/TR/2006/REC-xml11-20060816/">More information about XML</a>
+     * @see <a href="https://github.com/javadev/underscore-java">Conversion rules</a>
+     */
+    public String convertXMLtoJson(String xml){
+        return U.xmlToJson(xml);
+    }
 }
