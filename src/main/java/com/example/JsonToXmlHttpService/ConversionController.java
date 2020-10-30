f996a1816179ec561016ba48aa211eb8bfd9d405
Uniiquee <44533127+Uniiquee@users.noreply.github.com>
2020-10-30 08:23:19 +0100

    Add conversion endpoint implementation

---
+++ src/main/java/com/example/JsonToXmlHttpService/ConversionController.java
@@ -0,0 +1,28 @@
+package com.example.JsonToXmlHttpService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@RestController
+@RequestMapping(value = "/")
+public class ConversionController {
+
+    private final static Logger LOGGER = Logger.getLogger(ConversionController.class.getName());
+
+    @Autowired
+    JsonXmlConversionService jsonXmlConversionService;
+
+    @ResponseBody
+    @RequestMapping(value = "/convertJsonToXML", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> convertJsonToXML(@RequestBody String json) {
+        LOGGER.log(Level.INFO, "Should convert to xml:" + json);
+        String xml = jsonXmlConversionService.convertJsonToXML(json);
+        LOGGER.log(Level.INFO, "Resulting xml:" + xml);
+        return ResponseEntity.ok(xml);
+    }
+}
