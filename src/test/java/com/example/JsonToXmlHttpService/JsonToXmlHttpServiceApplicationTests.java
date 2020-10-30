12c058a94fa00f5afc554abc4c9cfe1635ef1e94
Uniiquee <44533127+Uniiquee@users.noreply.github.com>
2020-10-30 06:16:07 +0100

    Add Spring Boot project

---
+++ src/test/java/com/example/JsonToXmlHttpService/JsonToXmlHttpServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.JsonToXmlHttpService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonToXmlHttpServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
