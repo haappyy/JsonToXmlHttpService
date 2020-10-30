fb2796a080a7ba78bf99a4f8fafb3e3c06e937f1
Uniiquee <44533127+Uniiquee@users.noreply.github.com>
2020-10-30 06:49:33 +0100

    Add example Test to Hello World controller

--- src/test/java/com/example/JsonToXmlHttpService/JsonToXmlHttpServiceApplicationTests.java
+++ src/test/java/com/example/JsonToXmlHttpService/JsonToXmlHttpServiceApplicationTests.java
@@ -1,13 +1,30 @@
 package com.example.JsonToXmlHttpService;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+@EnableAutoConfiguration
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
 class JsonToXmlHttpServiceApplicationTests {
 
+	@Autowired
+	private MockMvc mockMvc;
+
 	@Test
-	void contextLoads() {
+	void getAuthorTest() throws Exception {
+		mockMvc.perform(get("/helloWorld").accept(MediaType.ALL))
+				.andDo(print())
+				.andExpect(content().string("Hello World"));
 	}
 
 }
